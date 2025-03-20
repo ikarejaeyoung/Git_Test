2ff50b4d888175035a1a7a9a5cfa0e91cc33c32d
ikarejaeyoung <wodud04277@gmail.com>
2025-03-20 12:51:46 +0900

    Fix lyrics
    가사 수정을 했습니다.

--- GitStudy/GitStudy/Program.cs
+++ GitStudy/GitStudy/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("단지 조금 어리단건");
+            Console.Write("단지 조금 더 어리단건");
             Console.WriteLine("Beenzino - Always awake");
             Console.WriteLine();
         }
